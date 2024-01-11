fd78311051a5327cbffce977190de2016ee395a3
Ruslan Illienko <ruslan.illienko@gmail.com>
2024-01-11 22:56:02 +0200

    Add project files.

---
+++ TelegramBotApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// add library
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+// add object reciever
+var token = "6768413846:AAHTqI_fwuU8GspCDtMfEvWuYtiAFqQsVBA";
+var botClientMain = new TelegramBotClient(token);
+
+var botInfo = await botClientMain.GetMeAsync();
+
+Console.WriteLine(botInfo.Id);
+Console.WriteLine(botInfo.Username);
+
+botClientMain.StartReceiving(
+    updateHandler: OnUpdateHandler,
+    pollingErrorHandler: OnPoolErrorHandler);
+
+Console.ReadLine();
+
+async Task OnUpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    var chatId = update.Message.Chat.Id;
+    string message = "Hello";
+    await botClient.SendTextMessageAsync(chatId, message);
+    await Task.CompletedTask;
+}
+
+async Task OnPoolErrorHandler(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    await Task.CompletedTask;
+}
